d75b7fa605727da65d9e1b1a949ffb2bc7ec0306
Lorenz Hörburger <e12024737@student.tuwien.ac.at>
2023-10-20 15:23:17 +0200

    add more

--- src/main/java/com/gcc/ccc/CccApplication.java
+++ src/main/java/com/gcc/ccc/CccApplication.java
@@ -1,13 +1,45 @@
 package com.gcc.ccc;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 @SpringBootApplication
-public class CccApplication {
+public class CccApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CccApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		List<String> data = readFile("level1/level1_1.in");
+		System.out.println(data);
+	}
+
+	private List<String> readFile(String input) {
+		ClassLoader classLoader = getClass().getClassLoader();
+		List<String>  data = new ArrayList<>();
+		try {
+			File myObj = new File(classLoader.getResource(input).getFile());
+			Scanner myReader = new Scanner(myObj);
+			while (myReader.hasNextLine()) {
+				data.add(myReader.nextLine());
+				System.out.println(data);
+			}
+			myReader.close();
+			return data;
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
