c1b595ed7936f5198b60f8da3861eb6a74a93147
Lorenz Hörburger <e12024737@student.tuwien.ac.at>
2023-10-20 14:57:10 +0200

    init

---
+++ src/main/java/com/gcc/ccc/CccApplication.java
@@ -0,0 +1,13 @@
+package com.gcc.ccc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gcc/ccc/CccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcc.ccc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
